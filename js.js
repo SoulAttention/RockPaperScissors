6341087d1993b26e21c228d281d41cfe2843b045
soulattention <emmaceplecha@outlook.com>
2022-02-19 15:49:01 -0800

    Replaced function of game

--- js.js
+++ js.js
@@ -1,57 +1,45 @@
 
-
-let playerSelection = prompt("write rock, paper or scissors");
-const choices = ["rock", "paper", "scissors"];
+let choices = ["rock", "paper", "scissors"];
+let playerSelection = prompt("write rock, paper or scissors", "");
 let computerSelection = computerPlay(choices);
+let computerScore = 0;
+let playerScore = 0;
+
+
 
 function computerPlay(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function game(computerSelection, playerSelection) {
-    switch(computerSelection > playerSelection) {
-        case "rock" > "paper":
-            return ("You Win Paper beats Rock!");
-            break;
-        case "scissors" > "paper":
-            return ("You lose scissors beats paper!");
-            break;
-        case "paper" > "paper":
-            return ("Its a tie!");
-            break;
-        case "rock" > "rock":
-            return ("Its a tie!!");
-            break;
-        case "scissors" > "rock":
-            return ("You win rock beats scissors!");
-            break;
-        case "paper" > "rock":
-            return ("You lose paper beats rock!"); 
-            break;   
-        case "rock" > "scissors":
-            return ("You lose rock beats scissors!");
-            break;
-        case "scissors" > "scissors":
-            return ("Its a tie!");
-            break;
-        case "paper" > "scissors":
-            return ("You win scissors beats paper!");
-            break;
-        default:
-            return ("uh oh something went wrong");
+function game (computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        ++computerScore
+        ++playerScore
+        alert("Its a tie!")
+    } else if ( 
+        (computerSelection == "rock" && playerSelection == "paper") ||
+        (computerSelection == "scissors" && playerSelection == "rock") ||
+        (computerSelection == "paper" && playerSelection == "scissors")
+    ) {
+        ++playerScore
+        alert("You Win!")
+
+    } else if (
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "scissors" && computerSelection == "rock") ||
+        (playerSelection == "paper" && computerSelection == "scissors")
+    ){
+        ++computerScore
+        alert("You lost")
+
     }
 }
+console.log(computerScore);
+console.log(playerScore);
 
 let result = game(computerPlay, playerSelection);
 
 
-function realGame(result) {
-    for (let i = 1; i <= 5; i++) {
-        let playerSelection = prompt("write rock, paper or scissors");
-        let computerSelection = computerPlay(choices);
-        console.log(game(computerPlay, playerSelection), i); 
-        alert(result);
-        }
-    }
+
 
 
