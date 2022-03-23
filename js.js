e37ed4413546713a3c62d2a75cad88d321b3e247
soulattention <emmaceplecha@outlook.com>
2022-03-23 13:14:43 -0700

    Added looping function for game

--- js.js
+++ js.js
@@ -1,60 +1,58 @@
 //  Declaring Variables
 const CHOICES = ["rock", "paper", "scissors"];
-let playerSelection = prompt("Please choose rock, paper, or scissors", "").toLowerCase();
-let computerSelection = computerChoice(CHOICES);
 let playerScore = 0;
 let computerScore = 0;
 
-// Computer choosing rock, paper, or scissors
-function computerChoice(CHOICES) {
-   return CHOICES[Math.floor(Math.random() * CHOICES.length)];
-}
+game();
 
+function computerChoice(CHOICES) {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+ }
 // Determining winner
 function playRound(computerSelection, playerSelection) {
-
+    
     if (
         (computerSelection == "rock" && playerSelection == "scissors") ||
         (computerSelection == "paper" && playerSelection == "rock") ||
         (computerSelection == "scissors" && playerSelection == "paper")
     ) {
-        computerScore = ++computerScore
-        return `You lose! ${computerSelection} beats ${playerSelection}` 
+        computerScore = ++computerScore;
+        alert(`You lose! ${computerSelection} beats ${playerSelection}`);
         
-
     } else if (
 
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")
     ) {
-        playerScore = ++playerScore
-        return `You Win! ${playerSelection} beats ${computerSelection}`
+        playerScore = ++playerScore;
+        alert(`You Win! ${playerSelection} beats ${computerSelection}`);
         
-    
     } else if (playerSelection == computerSelection) {
-        return `Its a tie! ${playerSelection} and ${computerSelection}`
+        alert(`Its a tie! ${playerSelection} and ${computerSelection}`);
         
     } else {
-        return `${playerSelection} is not a valid entry`
+        alert(`${playerSelection} is not a valid entry`);
     }
 }
 
 
 
+
 // Best of 5 rounds
- function game(playRound) {
-    for (let i = 0; computerScore || playerScore < 5; i++){
-        
-       playRound
-
+function game() {
     
-     }
+    while (computerScore < 5 && playerScore < 5) {
+        
+        let playerSelection = prompt("Please choose rock, paper, or scissors", "").toLowerCase();
+        let computerSelection = computerChoice(CHOICES);
+        playRound(computerSelection, playerSelection);
+        
+        let score = `The score is ${playerScore} to ${computerScore}.`
+        alert(score);
+
+        
+            
+    }
+    
  }
-
-console.log(playRound(computerSelection, playerSelection))
-//
-
-console.log(playerScore);
-console.log(computerScore);
-
